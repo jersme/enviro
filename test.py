1f5ffc2bb1812729b9e15c606236d59b3095f5d4
Jeroen <jersme@users.noreply.github.com>
2023-08-05 22:25:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
 import time
+import json
 from enviroplus import gas
-import logging
 
-logging.basicConfig(
-    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+class JSONLogger():
+    def __init__(self, filename):
+        self.filename = filename
 
-logging.info("""gas.py - Print readings from the MICS6814 Gas sensor.
+    def log(self, data):
+        with open(self.filename, 'a') as f:
+            json.dump(data, f)
+            f.write('\n')
+
+json_logger = JSONLogger('log.json')
+
+print("""gas.py - Print readings from the MICS6814 Gas sensor.
 
 Press Ctrl+C to exit!
 
@@ -18,7 +24,13 @@ Press Ctrl+C to exit!
 try:
     while True:
         readings = gas.read_all()
-        logging.info(readings)
+        # convert the readings object to a dictionary if it's not
+        if not isinstance(readings, dict):
+            readings = readings.__dict__
+        # add a timestamp to your readings
+        readings["timestamp"] = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
+        json_logger.log(readings)
         time.sleep(1.0)
 except KeyboardInterrupt:
     pass
+
