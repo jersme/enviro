dfb904fd69e8d89b83eff101870ce9bae3e78f8c
Jeroen <jersme@users.noreply.github.com>
2023-08-06 13:30:30 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,65 +10,58 @@ from PIL import Image, ImageDraw, ImageFont
 from fonts.ttf import RobotoMedium as UserFont
 from pms5003 import PMS5003
 
-# BME280 and SMBus setup
-bus = SMBus(1)
-bme280 = BME280(i2c_dev=bus)
-
-# LTR559 setup
 try:
     from ltr559 import LTR559
     ltr559 = LTR559()
 except ImportError:
     ltr559 = None
 
+# BME280 and SMBus setup
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+
 # PMS5003 setup
 pms5003 = PMS5003(device='/dev/ttyAMA0', baudrate=9600, pin_enable=22, pin_reset=27)
 
+# CPU Temperature function
 def get_cpu_temperature():
     with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
         temp = f.read()
         temp = int(temp) / 1000.0
     return temp
 
-factor = 2.25
+factor = 2.25  
 cpu_temps = [get_cpu_temperature()] * 5
 
-# Display setup
+# ST7735 Display setup
 disp = ST7735.ST7735(port=0, cs=1, dc=9, backlight=12, rotation=270, spi_speed_hz=10000000)
-disp.begin()
+disp.begin()  
 WIDTH = disp.width
 HEIGHT = disp.height
+
+# SQLite setup
+conn = sqlite3.connect('values_lindenhoevestraat.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS readings
+    (timestamp TEXT, oxidising REAL, reducing REAL, nh3 REAL, lux REAL, proximity REAL, compensated_temperature REAL, pressure REAL, humidity REAL, pm1 REAL, pm2_5 REAL, pm10 REAL)
+''')
 
 font_size = 10
 font = ImageFont.truetype(UserFont, font_size)
 text_colour = (255, 255, 255)
 back_colour = (0, 170, 170)
 
-# SQLite setup
-conn = sqlite3.connect('values_lindenhoevestraat.db')
-cursor = conn.cursor()
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS readings
-    (timestamp TEXT, oxidising REAL, reducing REAL, nh3 REAL, lux REAL, proximity REAL, compensated_temperature REAL, pressure REAL, humidity REAL, pm1 REAL, pm2_5 REAL, pm10 REAL)
-''')
-
-print("Press Ctrl+C to exit!")
-
 messages = []
 message_index = 0
-
+reading_count = 0
 last_update_time = time.time()
 
 try:
     while True:
         gas_readings = gas.read_all()
-        
-        if ltr559:
-            lux = ltr559.get_lux()
-            prox = ltr559.get_proximity()
-        else:
-            lux, prox = 0, 0
+        lux = ltr559.get_lux() if ltr559 else 0
+        prox = ltr559.get_proximity() if ltr559 else 0
 
         cpu_temp = get_cpu_temperature()
         cpu_temps = cpu_temps[1:] + [cpu_temp]
@@ -78,44 +71,36 @@ try:
 
         pressure = bme280.get_pressure()
         humidity = bme280.get_humidity()
+        
+        pm_data = pms5003.read()
 
-        pms_data = pms5003.read()
-        pm1 = pms_data.pm_ug_per_m3(1.0)
-        pm2_5 = pms_data.pm_ug_per_m3(2.5)
-        pm10 = pms_data.pm_ug_per_m3(10)
-
-        data_tuple = (time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()), gas_readings.oxidising, gas_readings.reducing, gas_readings.nh3, lux, prox, comp_temp, pressure, humidity, pm1, pm2_5, pm10)
-        print(data_tuple)
-
+        data_tuple = (time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()), gas_readings.oxidising, gas_readings.reducing, gas_readings.nh3, lux, prox, comp_temp, pressure, humidity, pm_data.pm_ug_per_m3(1.0), pm_data.pm_ug_per_m3(2.5), pm_data.pm_ug_per_m3(10))
         cursor.execute('INSERT INTO readings VALUES (?,?,?,?,?,?,?,?,?,?,?,?)', data_tuple)
         conn.commit()
 
-        time_since_update = (time.time() - last_update_time) / 60
+        reading_count += 1
+        time_since_update = int((time.time() - last_update_time) / 60) # in minutes
+        last_update_time = time.time()
 
-        messages = [
-            "Ox: {:.2f}".format(gas_readings.oxidising),
-            "Red: {:.2f}".format(gas_readings.reducing),
-            "NH3: {:.2f}".format(gas_readings.nh3),
-            "Lux: {:.2f}".format(lux),
-            "Prox: {:.2f}".format(prox),
-            "Temp: {:.2f} *C".format(comp_temp),
-            "Pressure: {:.2f} hPa".format(pressure),
-            "Humidity: {:.2f} %".format(humidity),
-            "PM1: {:.2f}".format(pm1),
-            "PM2.5: {:.2f}".format(pm2_5),
-            "PM10: {:.2f}".format(pm10)
-        ]
-
+        messages = ["Ox: {:.2f}".format(gas_readings.oxidising), 
+                    "Red: {:.2f}".format(gas_readings.reducing), 
+                    "NH3: {:.2f}".format(gas_readings.nh3), 
+                    "Lux: {:.2f}".format(lux), 
+                    "Prox: {:.2f}".format(prox), 
+                    "Temp: {:.2f} *C".format(comp_temp), 
+                    "Pressure: {:.2f} hPa".format(pressure),
+                    "Humidity: {:.2f} %".format(humidity),
+                    "PM1: {:.2f}".format(pm_data.pm_ug_per_m3(1.0)),
+                    "PM2.5: {:.2f}".format(pm_data.pm_ug_per_m3(2.5)),
+                    "PM10: {:.2f}".format(pm_data.pm_ug_per_m3(10))]
+                    
         img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
         draw = ImageDraw.Draw(img)
-        draw.rectangle((0, 0, 160, 80), back_colour)
+        draw.rectangle((0, 0, WIDTH, HEIGHT), back_colour)
+        
+        draw.text((0, 0), messages[message_index], font=font, fill=text_colour)
+        draw.text((0, HEIGHT - font_size), "Last: {}m | Count: {}".format(time_since_update, reading_count), font=font, fill=text_colour)
 
-        y = (HEIGHT / 3) - (font_size / 2)
-        draw.text((0, y), messages[message_index], font=font, fill=text_colour)
-        
-        # Drawing the time since the last update at the bottom
-        draw.text((0, HEIGHT - font_size - 5), "Updated: {:.1f} min ago".format(time_since_update), font=font, fill=text_colour)
-        
         disp.display(img)
 
         message_index = (message_index + 1) % len(messages)
